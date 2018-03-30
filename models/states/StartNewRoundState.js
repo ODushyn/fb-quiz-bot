f00d422bc4207b6af9594a83ebb993d4960e3fe6
Oleksandr Dushyn <wo.dushyn@gmail.com>
2018-03-30 23:33:54 +0200

    divide game into rounds (#11)

--- models/states/StartNewRoundState.js
+++ models/states/StartNewRoundState.js
@@ -3,7 +3,6 @@ module.exports = StartNewRoundState;
 function StartNewRoundState() {
     this.name = 'START_NEW_ROUND';
     this.init = function (player) {
-        player.sendTextMessage("You've got 30 sec to give an answer. \n For a new question type '-'");
         player.getHandler().startRound();
         player.changeState(new WaitingForQuizAnswerState());
     };
