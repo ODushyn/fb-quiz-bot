5eb00c10b7fe76eb4441cc8386b6a8af33624e7a
e211320 <oleksander.dushyn@evry.com>
2018-03-30 18:17:32 +0200

    add logging rotation and separate file for errors

--- common/logger.js
+++ common/logger.js
@@ -1,4 +1,5 @@
 const winston = require('winston');
+require('winston-daily-rotate-file');
 
 let config = winston.config;
 let logger = new (winston.Logger)({
@@ -6,25 +7,37 @@ let logger = new (winston.Logger)({
         new (winston.transports.File)({
             filename: 'app.log',
             level: 'info',
-            timestamp: () => {
-                return new Date().toLocaleString("en-US");
-            },
-            formatter: function(options) {
-                // - Return string will be passed to logger.
-                // - Optionally, use options.colorize(options.level, <string>) to
-                //   colorize output based on the log level.
-                return options.timestamp() + ' ' +
-                    config.colorize(options.level, options.level.toUpperCase()) + ' ' +
-                    (options.message ? options.message : '') +
-                    (options.meta && Object.keys(options.meta).length ? '\n\t'+ JSON.stringify(options.meta) : '' );
-            }
+            datePattern: 'YYYY-MM-DD-HH',
+            zippedArchive: true,
+            maxSize: '20m',
+            maxFiles: '14d',
+            timestamp: timestamp,
+            formatter: formatter
         }),
-        new (winston.transports.Console)({ level: 'error' })
+        new (winston.transports.File)({
+            name: 'error-file',
+            filename: 'error.log',
+            timestamp: timestamp,
+            formatter: formatter,
+            level: 'error'
+        }),
+        new (winston.transports.Console)({level: 'error'})
     ],
     exceptionHandlers: [
-        new (winston.transports.File)({ filename: 'exceptions.log'})
+        new (winston.transports.File)({filename: 'exceptions.log'})
     ],
     exitOnError: false
 });
+
+function formatter(options) {
+    return options.timestamp() + ' ' +
+        config.colorize(options.level, options.level.toUpperCase()) + ' ' +
+        (options.message ? options.message + ' ' : '') +
+        (options.meta && Object.keys(options.meta).length ? '\n\t' + JSON.stringify(options.meta) : '' );
+}
+
+function timestamp() {
+    return new Date().toLocaleString("en-US");
+}
 
 module.exports = logger;
