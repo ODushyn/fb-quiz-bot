db014b2e7a20dc73fbe801907144aec3516a8334
odushyn <oleksander.dushyn@evry.com>
2018-04-19 21:08:38 +0200

    update strat new round textt

--- models/states/StartNewRoundState.js
+++ models/states/StartNewRoundState.js
@@ -4,7 +4,8 @@ function StartNewRoundState() {
     this.name = 'START_NEW_ROUND';
     this.init = function (player) {
         player.setHandler(new MultiChoiceHandler(player));
-        player.addTextMessage("You've got 30 sec for each question.\nType '!' to finish game at any time.");
+        player.addTextMessage("- You've got 30 sec for each question.");
+        player.addTextMessage("- Type '!' to finish game at any time.");
         player.getHandler().startRound();
         player.changeState(new WaitingForQuizAnswerState());
     };
