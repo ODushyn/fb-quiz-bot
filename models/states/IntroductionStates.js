238ba1e8d59b75a48e849ac641f4eed3648bf3ce
odushyn <oleksander.dushyn@evry.com>
2018-04-19 22:43:10 +0200

    fix greeting message

--- models/states/IntroductionStates.js
+++ models/states/IntroductionStates.js
@@ -3,47 +3,46 @@ exports.WaitingQuestionsNumberPerRoundState = WaitingQuestionsNumberPerRoundStat
 exports.WaitingTypeState = WaitingTypeState;
 
 function WaitingFirstMessageState() {
-    this.name = 'WAITING_FIRST_MESSAGE';
-    this.transition = function (player) {
-        player.changeState(new WaitingQuestionsNumberPerRoundState())
-    };
+  this.name = 'WAITING_FIRST_MESSAGE';
+  this.transition = function(player) {
+    player.changeState(new WaitingQuestionsNumberPerRoundState());
+  };
 }
 
 function WaitingQuestionsNumberPerRoundState() {
-    this.name = 'WAITING_QUESTIONS_NUMBER';
-    this.init = function (player) {
-        player.setHandler(new IntroductionHandler(player));
-        player.addTextMessage(INTRODUCTION_MESSAGES.GREETING)
-              .addTextMessage(INTRODUCTION_MESSAGES.ASK_QUESTIONS_NUMBER_PER_ROUND);
-        player.flushMessages();
-    };
-    this.transition = function (player) {
-        let questionsNumber = player.getMessage();
-        let valid = player.getHandler().processQuestionsNumber(questionsNumber);
-        if (valid) {
-            player.setQuestionsNumberPerRound(questionsNumber);
-            player.changeState(new WaitingTypeState())
-        } else {
-            player.addTextMessage('Type 1 or 2').flushMessages();
-        }
-    };
+  this.name = 'WAITING_QUESTIONS_NUMBER';
+  this.init = function(player) {
+    player.setHandler(new IntroductionHandler(player));
+    player.addTextMessage(INTRODUCTION_MESSAGES.ASK_QUESTIONS_NUMBER_PER_ROUND);
+    player.flushMessages();
+  };
+  this.transition = function(player) {
+    let questionsNumber = player.getMessage();
+    let valid = player.getHandler().processQuestionsNumber(questionsNumber);
+    if (valid) {
+      player.setQuestionsNumberPerRound(questionsNumber);
+      player.changeState(new WaitingTypeState());
+    } else {
+      player.addTextMessage('Type 1 or 2').flushMessages();
+    }
+  };
 }
 
 function WaitingTypeState() {
-    this.name = 'WAITING_TYPES';
-    this.init = function (player) {
-        player.addTextMessage(INTRODUCTION_MESSAGES.ASK_TYPES).flushMessages();
-    };
-    this.transition = function (player) {
-        let type = player.getMessage();
-        let valid = player.getHandler().processType(type);
-        if (valid) {
-            player.setType(type);
-            player.changeState(new StartNewRoundState());
-        } else {
-            player.addTextMessage('Type 1, 2 or 3').flushMessages();
-        }
-    };
+  this.name = 'WAITING_TYPES';
+  this.init = function(player) {
+    player.addTextMessage(INTRODUCTION_MESSAGES.ASK_TYPES).flushMessages();
+  };
+  this.transition = function(player) {
+    let type = player.getMessage();
+    let valid = player.getHandler().processType(type);
+    if (valid) {
+      player.setType(type);
+      player.changeState(new StartNewRoundState());
+    } else {
+      player.addTextMessage('Type 1, 2 or 3').flushMessages();
+    }
+  };
 }
 
 let IntroductionHandler = require('../../handlers/IntroductionHandler.js');
--- webHook.js
+++ webHook.js
@@ -1,16 +1,19 @@
 const StateContext = require('./StateContext.js');
-const WaitingFirstMessageState = require('./models/states/IntroductionStates.js').WaitingFirstMessageState;
+const WaitingFirstMessageState = require('./models/states/IntroductionStates.js')
+  .WaitingFirstMessageState;
 const Player = require('./models/Player.js');
 const logger = require('./common/logger');
+const INTRODUCTION_MESSAGES = require('./constants/const.js').INTRODUCTION;
 
 let context = {};
 
-exports.proceed = function(playerId, message){
-    logger.info('playerId: %s, text: %s', playerId, message);
-    if (!context[playerId]) {
-        logger.info('Create player: %s', playerId);
-        let stateContext = new StateContext(new WaitingFirstMessageState());
-        context[playerId] = new Player(playerId, stateContext);
-    }
-    context[playerId].process(message);
+exports.proceed = function(playerId, message) {
+  logger.info('playerId: %s, text: %s', playerId, message);
+  if (!context[playerId]) {
+    logger.info('Create player: %s', playerId);
+    let stateContext = new StateContext(new WaitingFirstMessageState());
+    context[playerId] = new Player(playerId, stateContext);
+    context[playerId].addTextMessage(INTRODUCTION_MESSAGES.GREETING);
+  }
+  context[playerId].process(message);
 };
